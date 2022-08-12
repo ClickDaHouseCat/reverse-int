5b025551b59326e0f3f871ee2c7f86a6be63ae59
ClickDaHouseCat <monsterhunter@list.ru>
2022-08-13 00:29:09 +0300

    feat: complete reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 module.exports = function reverse (n) {
-  
+    if (n > 0){
+        let a = String(n).split('').reverse().join('');
+            return +a;
+        } else {
+            a = String(n).split('').slice(1).reverse().join('');
+            return +a;
+        }
 }
